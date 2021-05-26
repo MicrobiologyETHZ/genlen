56cccfe5c2ccc9da34f7faf9d0f04bbfb42c1b20
cmfield <fieldc@ethz.ch>
2021-05-26 11:04:41 +0200

    minor fixes

--- genlen.py
+++ genlen.py
@@ -36,19 +36,21 @@ def __main__():
     # Read in protein sequences
     proteins = SeqIO.parse("{}.faa".format(outfix), 'fasta')
 
+    # Determine gene lengths
+    gene_lengths = {protein.id:len(protein) for protein in proteins}
+
     if args.blast:
         # Blast protein sequences against reference database
         hits = blast_genes("{}.faa".format(outfix), args.db, args.threads)
 
-    # Filter hits
-    good_hits = {k:v for k,v in hits.items() if float(v[2]) >= args.cutoff}
+        # Filter hits
+        good_hits = {k:v for k,v in hits.items() if float(v[2]) >= args.cutoff}
 
-    # Determine gene lengths
-    gene_lengths = {protein.id:len(protein) for protein in proteins}
-    hit_lengths = {k:int(v[1]) for k,v in good_hits.items()}
+        # Determine hit lengths
+        hit_lengths = {k:int(v[1]) for k,v in good_hits.items()}
 
-    # Calculate ratios
-    length_ratios = {k:gene_lengths[k]/hit_lengths[k] for k in hit_lengths.keys()}
+        # Calculate ratios
+        length_ratios = {k:gene_lengths[k]/hit_lengths[k] for k in hit_lengths.keys()}
 
     # Output
     with open('{}_results.txt'.format(outfix), 'w') as fo:
