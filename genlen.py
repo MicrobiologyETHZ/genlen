e2bbcce27be63e77ded7877d3e154d7ffebdba25
cmfield <fieldc@ethz.ch>
2022-11-07 16:02:53 +0100

    output now includes db hit name

--- genlen.py
+++ genlen.py
@@ -2,12 +2,12 @@ import argparse, os, subprocess
 from Bio import SeqIO
 import matplotlib.pyplot as plt
 
-def blast_genes(faa, db, threads):
-    arg = f'diamond blastp --threads {threads} --max-target-seqs 1 --db {db} --query {faa} --outfmt 6 qseqid sseqid slen pident'
+def align_genes(faa, db, threads):
+    arg = f'diamond blastp --threads {threads} --max-target-seqs 1 --db {db} --query {faa} --outfmt 6 qseqid sseqid qlen slen pident'
     results = subprocess.check_output(arg, shell=True).decode()
 
     hits = [line.split("\t") for line in results.splitlines()]
-    hits = {hit[0]:hit[1:] for hit in hits}
+    hits = {hit[0]:hit for hit in hits}
 
     return(hits)
 
@@ -26,26 +26,30 @@ def plot_hist(outfix, mode, values):
         plt.xlabel("Gene length ratio")
         plt.savefig(f'{outfix}_gene_ratios.png')
 
-def __main__():
+if True:
+#ef __main__():
     parser = argparse.ArgumentParser(description='Call genes with prodigal, calculate gene length distribution and optionally compare to the nearest BLAST hits.')
     parser.add_argument('contigs', metavar='contigs_file', help='File containing contigs in fasta format.')
-    parser.add_argument('--blast', action='store_true', help='Compare gene lengths to nearest BLAST hits in db.')
-    parser.add_argument('--db', metavar='ref_db', default='/nfs/cds/Databases/UNIPROT/uniprot_trembl_bacteria.dmnd', help='Diamond reference database for gene alignment.')
+    parser.add_argument('--align', action='store_true', help='Find best hits in db with DIAMOND.')
+    parser.add_argument('--db', metavar='ref_db', default='/nfs/cds/Databases/UNIPROT/uniprot_trembl_bacteria.dmnd', help='DIAMOND reference database for gene alignment.')
     parser.add_argument('--cutoff', metavar='id_cutoff', default=97.0, help='Identity cutoff for a valid hit. Default: 97.0.')
-    parser.add_argument('--plot', action='store_true', help='Plot histograms of gene length and ratio (if --blast).')
+    parser.add_argument('--plot', action='store_true', help='Plot histograms of gene length and ratio (if --align).')
+    parser.add_argument('-s', '--stop', action='store_true', default=False, help='Database contains stop codons. Default: False.')
+    parser.add_argument('-o', '--out', metavar='outdir', help='Output directory. Defaults to genlen_results in the same directory as the contigs file')
     parser.add_argument('-t,', '--threads', metavar='threads', default=16, help='Number of compute threads. Default: 16.')
 
     args = parser.parse_args()
 
     # Determine file names
-    dir = os.path.split(args.contigs)[0]
-    outdir = os.path.join(dir, "genlen_results")
+    if not args.out:
+        dir = os.path.split(args.contigs)[0]
+        args.out = os.path.join(dir, "genlen_results")
 
     prefix = os.path.splitext(os.path.split(args.contigs)[1])[0]
-    outfix = os.path.join(outdir, prefix)
+    outfix = os.path.join(args.out, prefix)
 
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
+    if not os.path.exists(args.out):
+        os.mkdir(args.out)
 
     # Call genes with prodigal
     prodigal_output = subprocess.check_output(f'prodigal -c -i {args.contigs} -o {outfix}.gff -a {outfix}.faa -d {outfix}.fna -f gff',shell=True).decode()
@@ -53,29 +57,30 @@ def __main__():
     # Read in protein sequences
     proteins = SeqIO.parse(f'{outfix}.faa', 'fasta')
 
-    # Determine gene lengths
-    gene_lengths = {protein.id:len(protein)-1 for protein in proteins}
-
-    if args.blast:
+    if args.align:
         # Blast protein sequences against reference database
-        hits = blast_genes(f'{outfix}.faa', args.db, args.threads)
+        hits = align_genes(f'{outfix}.faa', args.db, args.threads)
 
         # Filter hits
-        good_hits = {k:v for k,v in hits.items() if float(v[2]) >= args.cutoff}
+        good_hits = {k:v for k,v in hits.items() if float(v[4]) >= args.cutoff}
 
-        # Determine hit lengths
-        hit_lengths = {k:int(v[1]) for k,v in good_hits.items()}
-
-        # Calculate ratios
-        length_ratios = {k:gene_lengths[k]/hit_lengths[k] for k in hit_lengths.keys()}
+        # Subtract stop codons from lengths then calculate length ratio
+        for hit in hits.values():
+            hit[2] = int(hit[2])-1
+            if args.stop:
+                hit[3] = int(hit[3])-1
+            else:
+                hit[3] = int(hit[3])
+            hit.append(hit[2]/hit[3])
 
     # Output
     with open(f'{outfix}_results.txt', 'w') as fo:
-        for k in gene_lengths.keys():
+        for protein in proteins:
             try:
-                fo.write(f'{k}\t{gene_lengths[k]}\t{hit_lengths[k]}\t{length_ratios[k]}\n')
+                towrite = '\t'.join(map(str, good_hits[protein.id]))
+                fo.write(f'{towrite}\n')
             except:
-                fo.write(f'{k}\t{gene_lengths[k]}\tNA\tNA\n')
+                fo.write(f'{protein.id}\tNA\t{len(protein)}\tNA\tNA\n')
 
     # Graphs
     if args.plot:
@@ -83,6 +88,6 @@ def __main__():
         if args.blast:
             plot_hist(outfix, "ratio", length_ratios.values())
 
-if __name__ == "__main__":
-    __main__()
+#if __name__ == "__main__":
+#    __main__()
 
